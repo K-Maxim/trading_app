a8213e932996a898ea87c0a03e284ce5c9e2ea30
K-Maxim <m.karpenko96@mail.ru>
2023-04-26 17:51:52 +0300

    add new table

--- migrations/env.py
+++ migrations/env.py
@@ -5,10 +5,16 @@ from sqlalchemy import pool
 
 from alembic import context
 
+import os
+import sys
+
+sys.path.append(os.path.join(sys.path[0], 'src'))
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
-from config import DB_HOST, DB_USER, DB_PASS, DB_PORT, DB_NAME
-from auth.model import metadata
+from src.config import DB_HOST, DB_USER, DB_PASS, DB_PORT, DB_NAME
+from src.auth.model import metadata as metadata_auth
+from src.operations.models import metadata as metadata_operations
 
 config = context.config
 
@@ -28,7 +34,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = metadata
+target_metadata = [metadata_auth, metadata_operations]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
