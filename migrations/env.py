44136096f3d454426c0e19f66fa26e583e86b301
K-Maxim <m.karpenko96@mail.ru>
2023-04-27 17:26:37 +0300

    refactories

--- migrations/env.py
+++ migrations/env.py
@@ -8,13 +8,16 @@ from alembic import context
 import os
 import sys
 
+
+from src.operations.models import *
+from src.auth.model import *
+from database import metadata
 sys.path.append(os.path.join(sys.path[0], 'src'))
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 from src.config import DB_HOST, DB_USER, DB_PASS, DB_PORT, DB_NAME
-from src.auth.model import metadata as metadata_auth
-from src.operations.models import metadata as metadata_operations
+
 
 config = context.config
 
@@ -34,7 +37,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = [metadata_auth, metadata_operations]
+target_metadata = metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- src/database.py
+++ src/database.py
@@ -1,4 +1,6 @@
 from typing import AsyncGenerator
+
+from sqlalchemy import MetaData
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeMeta, declarative_base
 
@@ -6,6 +8,8 @@ from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 Base: DeclarativeMeta = declarative_base()
+
+metadata = MetaData()
 
 engine = create_async_engine(DATABASE_URL)
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
