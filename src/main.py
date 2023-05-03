b963c2362084c2f121d9f941205c30409fa0b79d
K-Maxim <m.karpenko96@mail.ru>
2023-05-03 21:05:58 +0300

    CORS

--- src/main.py
+++ src/main.py
@@ -12,10 +12,15 @@ from redis import asyncio as aioredis
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from tasks.router import router as router_task
+from pages.router import router as router_pages
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="Trading app"
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 fastapi_users = FastAPIUsers[User, int](
     get_user_manager,
@@ -36,8 +41,21 @@ app.include_router(
 
 app.include_router(router_operation)
 app.include_router(router_task)
+app.include_router(router_pages)
 
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 @app.on_event("startup")
 async def startup():
     redis = aioredis.from_url("redis://localhost", encoding="utf8", decode_responses=True)
